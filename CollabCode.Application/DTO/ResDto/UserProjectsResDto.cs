e7a0776ef98a6e708ce961fc432e0690c73a0e94
Muhammed Suhail P <suhailpalakkal1@gmail.com>
2025-10-23 14:04:58 +0530

    User controll finised

---
+++ CollabCode.Application/DTO/ResDto/UserProjectsResDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CollabCode.CollabCode.Application.DTO.ResDto
+{
+    public class UserProjectsDto
+    {
+        public string? UserName { get; set; }
+        public List<ProjectDto> OwnedRooms { get; set; } = new();
+        public List<ProjectDto> JoinedRooms { get; set; } = new();
+    }
+
+    public class ProjectDto
+    {
+        public int Id { get; set; }
+        public string? ProjectName { get; set; }
+        public string? JoinCode { get; set; } = null;
+    }
+}
